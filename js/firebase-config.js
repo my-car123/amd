5b4eb4668f60008b63e75732c941b3bf2c251f7e
my-car123 <p.my123.car@gmail.com>
2026-06-10 18:24:45 +0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -7,7 +7,6 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebas
 import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 import { getDatabase, ref, set, get, child, push, query, orderByChild, equalTo, limitToFirst } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 
-// بيانات الاعتماد الرسمية الحية الخاصة بقاعدتك اللحظية
 const firebaseConfig = {
   apiKey: "AIzaSyDBHHGY_gVpm3NlXThqsC6ojTL9Je4xQ9w",
   authDomain: "car-moving-8b59e.firebaseapp.com",
@@ -18,17 +17,14 @@ const firebaseConfig = {
   appId: "1:332747318494:web:d5d61cd53f322a182f0e4f"
 };
 
-// تهيئة التطبيق المركزي وسحب رخص الاتصال
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
 
-// تثبيت ديمومة الجلسة للمشرفين لمدة 30 يوماً بشكل مستقر ومحمي داخلياً
 setPersistence(auth, browserLocalPersistence)
     .then(() => console.log("System Connection Secured: 30-Days Persistence Active."))
     .catch((err) => console.error("Persistence Configuration Failure:", err));
 
-// تصدير الكتل البرمجية لنافذة المتصفح لضمان الترابط الشامل بين الملفات
 window.firebaseAuth = auth;
 window.firebaseDB = db;
 window.dbTools = { ref, set, get, child, push, query, orderByChild, equalTo, limitToFirst };
