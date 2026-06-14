fd95c1156b4ab3b7c081b1184bb55707762e0bd5
my-car123 <p.my123.car@gmail.com>
2026-06-14 22:52:13 +0400

    Update firebase.js

--- js/config/firebase.js
+++ js/config/firebase.js
@@ -1,7 +1,5 @@
-// js/config/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDBHHGY_gVpm3NlXThqsC6ojTL9Je4xQ9w",
@@ -12,9 +10,15 @@ const firebaseConfig = {
   appId: "1:332747318494:web:d5d61cd53f322a182f0e4f"
 };
 
-// تهيئة التطبيق
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-// تصدير الخدمات لاستخدامها في بقية الملفات
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+// اختبار اتصال مباشر
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+getDocs(collection(db, "users")).then(() => {
+    console.log("SUCCESS: Firestore Connected!");
+}).catch(err => {
+    console.error("CRITICAL ERROR:", err.code, err.message);
+});
+
+export { db };
