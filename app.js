e335d00fea3d2bcc3c4f06d86a9de1873ad88b89
my-car123 <p.my123.car@gmail.com>
2026-06-19 13:57:40 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
-
 const firebaseConfig = { apiKey: "AIzaSyDBHHGY_gVpm3NlXThqsC6ojTL9Je4xQ9w", authDomain: "car-moving-8b59e.firebaseapp.com", projectId: "car-moving-8b59e", storageBucket: "car-moving-8b59e.firebasestorage.app", messagingSenderId: "332747318494", appId: "1:332747318494:web:d5d61cd53f322a182f0e4f" };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -9,5 +8,5 @@ document.getElementById('loginBtn').addEventListener('click', async () => {
     try {
         await signInWithEmailAndPassword(auth, document.getElementById('email').value, document.getElementById('password').value);
         window.location.href = "dashboard.html";
-    } catch (e) { alert("Login Failed: " + e.message); }
+    } catch (e) { alert("Access Denied"); }
 });
